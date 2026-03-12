36461b07c9c263452fec4e5fa23756f88796b642
su apt install kuma <maxxozou@gmail.com>
2026-03-12 18:09:56 +0100

    Add Create Extra Recipes mod scaffold
    Initialize a Fabric mod project for Create Extra Recipes: add Gradle build files (build.gradle, gradle.properties, settings), Gradle wrapper, .gitignore and LICENSE. Add mod sources and client entry points (CreateExtraRecipes, CreateExtraRecipesClient, data generator), mixin configs, icon asset, and a large collection of data JSONs (recipes and tags) under src/main/resources/data/create-extra-recipes to provide new crafting/blasting/compacting/mixing/crushing/filling/etc. recipes.

---
+++ src/client/java/net/kuma/craftgunpower/createExtraRecipes/client/CreateExtraRecipesClient.java
@@ -0,0 +1,10 @@
+package net.kuma.craftgunpower.createExtraRecipes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreateExtraRecipesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/kuma/craftgunpower/createExtraRecipes/CreateExtraRecipes.java
@@ -0,0 +1,11 @@
+package net.kuma.craftgunpower.createExtraRecipes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CreateExtraRecipes implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Create Extra Recipes Load !");
+    }
+}
